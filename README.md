f6dabaeceb193f19989d0b226d2d4eb2f99133fe
Daniel Lindau <d.lindau@gmail.com>
2024-06-19 15:30:09 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Migration Data Access Plugin Example
+
+This is a sample migration Data Access Plugin for the Curity Identity Server.
+
+It aims to demonstrate how two different Credential Managers can be used in migration purposes
+
+Please visit [curity.io](https://curity.io/) for more information about the Curity Identity Server.
---
+++ src/main/java/io/curity/identityserver/plugin/data/access/migration/MigrationCredentialDataAccessProvider.java
@@ -0,0 +1,48 @@
+package io.curity.identityserver.plugin.data.access.migration;
+
+import io.curity.identityserver.plugin.data.access.migration.config.MigrationDataAccessProviderConfiguration;
+import se.curity.identityserver.sdk.attribute.AuthenticationAttributes;
+import se.curity.identityserver.sdk.attribute.ContextAttributes;
+import se.curity.identityserver.sdk.attribute.SubjectAttributes;
+import se.curity.identityserver.sdk.datasource.CredentialDataAccessProviderFactory;
+import se.curity.identityserver.sdk.datasource.CredentialVerifyingDataAccessProvider;
+import se.curity.identityserver.sdk.service.ExceptionFactory;
+
+public final class MigrationCredentialDataAccessProvider implements CredentialVerifyingDataAccessProvider, CredentialDataAccessProviderFactory
+{
+    private final MigrationDataAccessProviderConfiguration _configuration;
+    private final ExceptionFactory _exceptionFactory;
+
+    public MigrationCredentialDataAccessProvider(MigrationDataAccessProviderConfiguration configuration,
+                                                 ExceptionFactory exceptionFactory)
+    {
+        _configuration = configuration;
+        _exceptionFactory = exceptionFactory;
+    }
+
+    @Override
+    public VerifyResult verify(SubjectAttributes subject, String password)
+    {
+        boolean isMigratedAndMatching = _configuration.getMigratedCredentialManager()
+                .verifyPassword(subject.getSubject(), password);
+
+        if (isMigratedAndMatching)
+        {
+            boolean isInLegacy = _configuration.getLegacyCredentialManager()
+                    .verifyPassword(subject.getSubject(), password);
+            if (isInLegacy)
+            {
+                // TODO: Move to new DS
+                return new VerifyResult.Accepted(AuthenticationAttributes.of(subject, ContextAttributes.empty()));
+            }
+            return new VerifyResult.Rejected("Not found in either DS");
+        }
+        return new VerifyResult.Accepted(AuthenticationAttributes.of(subject, ContextAttributes.empty()));
+    }
+
+    @Override
+    public SetResult set(SubjectAttributes subject, String password)
+    {
+        throw _exceptionFactory.methodNotAllowed();
+    }
+}
